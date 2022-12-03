4ce7865f3ca15a68d97fbb848a9f5a1df5bd4ea0
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-12-03 18:40:40 +0300

    created the height_entry

---
+++ BMI_Application/bmi.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import customtkinter
+from PIL import Image, ImageTk
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+
+root.title('Cipher - BMI Calculator')
+root.geometry("500x600")
+
+# Define Entry Boxes
+
+h_entry = customtkinter.CTkEntry(master=root, placeholder_text="Height in Inches",
+                                 width=200, height=30, border_width=1, corner_radius=10)
+h_entry.pack(pady=20)
+
+
+root.mainloop()
